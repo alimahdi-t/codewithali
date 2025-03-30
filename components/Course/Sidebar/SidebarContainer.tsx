aa156f3deece55584cb5fb8e459bf76e91469e96
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-03-30 15:09:45 +0330

    Make sidebar of courses page sticky

--- components/Course/Sidebar/SidebarContainer.tsx
+++ components/Course/Sidebar/SidebarContainer.tsx
@@ -3,9 +3,10 @@ import CategoryFilter from "@/components/Course/Sidebar/CategoryFilter";
 import { HiOutlineCodeBracket } from "react-icons/hi2";
 
 import { courseLevelFilter } from "@/constants/filters";
+
 const BlogCardSideBar = () => {
   return (
-    <div className="flex flex-col  gap-4 w-[250px]  max-md:hidden max-h-screen">
+    <div className="flex flex-col  gap-4 w-[250px]  max-md:hidden max-h-screen sticky top-4">
       <FilterToggle label="فقط دوره‌های رایگان" filterParam="isFree" />
       <FilterToggle label="درحال پیش فروش" filterParam="isPreOrder" />
 
