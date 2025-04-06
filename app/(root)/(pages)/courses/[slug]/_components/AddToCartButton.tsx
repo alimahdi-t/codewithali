65b2a33fc169e6415ad91ac7a5aea957afce619b
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-04-06 23:33:03 +0330

    - Show a toast when a user that not logged in tries to add item to cart.

--- app/(root)/(pages)/courses/[slug]/_components/AddToCartButton.tsx
+++ app/(root)/(pages)/courses/[slug]/_components/AddToCartButton.tsx
@@ -4,18 +4,28 @@ import { HiShoppingCart } from "react-icons/hi2";
 
 import { Button } from "@/components/ui/button";
 import { useCart } from "@/context/CartProvider";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { toast } from "sonner";
 
 interface Props {
   courseId: string;
 }
 
 export const AddToCartButton = ({ courseId }: Props) => {
+  const user = useCurrentUser();
+  const handleOnClick = () => {
+    if (!user) {
+      toast.info("لطفا ابتدا وارد حساب خود شوید.");
+      return;
+    }
+    addToCart(courseId);
+  };
   const { addToCart } = useCart();
   return (
     <Button
       size="lg"
       className="text-sm font-semibold dark:text-white flex items-center"
-      onClick={() => addToCart(courseId)}
+      onClick={handleOnClick}
     >
       <HiShoppingCart className="size-4.5 ml-1" />
       <p>افزودن به سبد خرید</p>
