4d215d4f12c73c84232fa1e867633d26ed80ea1b
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-04-13 23:33:09 +0330

    - Modify CommentStatus

--- app/(dashboards)/dashboard/admin/comments/_components/CommentStatus.tsx
+++ app/(dashboards)/dashboard/admin/comments/_components/CommentStatus.tsx
@@ -1,10 +1,20 @@
-import { CommentStatus as status } from "@prisma/client";
+import { CommentStatus as commentStatus } from "@prisma/client";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 interface Props {
-  status: status;
+  status: commentStatus;
 }
 
-const statusVariants: Record<status, { text: string; className: string }> = {
+const statusVariants: Record<
+  commentStatus,
+  { text: string; className: string }
+> = {
   PENDING: {
     text: "در انتظار تایید",
     className:
@@ -24,12 +34,32 @@ const statusVariants: Record<status, { text: string; className: string }> = {
 
 export const CommentStatus = ({ status }: Props) => {
   const variant = statusVariants[status];
-
   return (
-    <span
-      className={`${variant.className} px-2 inline-block py-1 rounded-xl text-xs font-light leading-5 text-center text-nowrap`}
+    <Menubar
+      asChild
+      className={`relative border-none shadow-none p-2 w-12`}
+      dir="rtl"
     >
-      {variant.text}
-    </span>
+      <MenubarMenu>
+        <MenubarTrigger className="rounded-xl p-0">
+          <span
+            className={`${variant.className} px-2 inline-block py-1 rounded-xl text-xs font-light leading-5 text-center text-nowrap`}
+          >
+            {variant.text}
+          </span>
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem className="flex items-center gap-1 px-2.5 py-2 cursor-pointer">
+            بدون تغییر
+          </MenubarItem>
+          <MenubarItem className="flex items-center gap-1 px-2.5 py-2 cursor-pointer">
+            تأیید
+          </MenubarItem>
+          <MenubarItem className="flex items-center gap-1 px-2.5 py-2 cursor-pointer">
+            رد
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 };
