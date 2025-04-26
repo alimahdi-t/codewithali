324f6e7101f4e965a322319b7281443d95c09e9f
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-04-27 01:35:05 +0330

    - style loading for dark mode

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { BeatLoader } from "react-spinners";
+import { useTheme } from "@/context/ThemeProvider";
 
 const RootLoading = () => {
+  const { mode } = useTheme();
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <BeatLoader size={24} />
+      <BeatLoader size={24} color={mode === "dark" ? "white" : "black"} />
     </div>
   );
 };
