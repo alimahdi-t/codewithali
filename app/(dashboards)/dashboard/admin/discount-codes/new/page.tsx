da0a730b68a8d6daa820348c73c85736f6030991
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-05-07 00:07:24 +0330

    add multiSelect feature to CreateDiscountCodeForm

--- app/(dashboards)/dashboard/admin/discount-codes/new/page.tsx
+++ app/(dashboards)/dashboard/admin/discount-codes/new/page.tsx
@@ -1,10 +1,13 @@
 import CreateDiscountCodeForm from "@/components/forms/CreateDiscountCodeForm";
+import { getCourseNames } from "@/actions/discount-codes/get-course-names.action";
 
-export default function NewDiscountCodePage() {
+export default async function NewDiscountCodePage() {
+  const response = await getCourseNames();
+  const { data } = response;
   return (
     <div className="w-full flex flex-col items-center py-12">
       <h2 className="font-bold text-xl mb-8">افزودن کد تخفیف</h2>
-      <CreateDiscountCodeForm />
+      <CreateDiscountCodeForm courses={data ?? []} />
     </div>
   );
 }
