9d01d7ced82522d107d1ae87be81e6088a3b096a
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-09-18 23:25:57 +0330

    Add a tags table.

--- app/(dashboards)/dashboard/admin/tags/page.tsx
+++ app/(dashboards)/dashboard/admin/tags/page.tsx
@@ -1,7 +1,26 @@
-const TagsPage = () => {
+import { DashboardPageHeader } from "@/components/dashboard/DashboardPageHeader";
+import { ClientToastWrapper } from "@/components/common/ClientToastWrapper";
+import { getAllTagsAction } from "@/actions/tags/get-all-tags.action";
+import { TagsTable } from "@/components/tables/TagsTable";
+
+const TagsPage = async () => {
+  const response = await getAllTagsAction();
+
+  if (response.error) {
+    return <ClientToastWrapper message={response.error} variant="error" />;
+  }
+  const { tags } = response;
   return (
-    <div>
-      <p>TagsPage</p>
+    <div className="w-full py-6">
+      <div className="mb-4">
+        <DashboardPageHeader
+          title="لیست تگ‌ها"
+          description="لیست تمام مقاله‌ها، برای مشاهده مشخصات آنها با کلیک بر روی آن وارد صحفه آن مقاله شوید."
+          buttonLabel="افزودن مقاله"
+        />
+      </div>
+
+      <TagsTable data={tags ?? []} />
     </div>
   );
 };
