d7ee10ad6b8a347d0c3bb9f2490fc40b6c29fd89
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-09-21 23:40:51 +0330

    Delete unused files

--- app/api/tags/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getTags } from "@/actions/getTags.action";
-
-export async function GET(request: NextRequest) {
-  const tags = await getTags();
-  return NextResponse.json(tags);
-}
