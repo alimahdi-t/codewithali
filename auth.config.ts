89956318b162b90b73b9405361ff637c478c611f
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-09-22 18:56:51 +0330

    add Google sign-in method

--- auth.config.ts
+++ auth.config.ts
@@ -3,6 +3,7 @@ import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "@/schema";
 import { getUserByEmail } from "@/data/user";
 import { comparePassword } from "@/utils/password";
+import Google from "next-auth/providers/google";
 
 export default {
   providers: [
@@ -25,6 +26,13 @@ export default {
 
         return null;
       },
+    }),
+    //
+    // GitHub({
+    //   allowDangerousEmailAccountLinking: true,
+    // }),
+    Google({
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
 } satisfies NextAuthConfig;
